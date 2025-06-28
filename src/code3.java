c6e3ff26e93d2ba1bdf02eb93994f6490c93eaf4
unknown <gdutt2304@gmail.com>
2025-06-28 13:46:48 +0530

    Static mehtods

---
+++ src/code3.java
@@ -0,0 +1,23 @@
+public class code3 {
+    public static void main(String[] args) {
+//        System.out.println(printMessage(12 , 89));
+//        System.out.println(printMessage(12 , 89));
+//
+//        int a = 34;
+//        int b = 34;
+//        System.out.println(a + b);
+//
+//        int a1 = 45;
+//        int a2 = 45;
+//        System.out.println(a1+a2);
+
+     int sum =   methodsClass.printMessage(12,98);
+        System.out.println(sum);
+    }
+//    public static int printMessage(int a , int b){
+//        System.out.println("Welcome...");
+//        int c = a + b;
+//        return  c;
+//    }
+
+}
---
+++ src/methodsClass.java
@@ -0,0 +1,7 @@
+public class methodsClass {
+    public static int printMessage(int a , int b){
+//        System.out.println("Welcome...");
+        int c = a + b;
+        return  c;
+    }
+}
